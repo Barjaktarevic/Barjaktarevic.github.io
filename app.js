15fb5e90eee54a4820b5a680ccfd13cf61d8695d
Uroš Barjaktarević <ubarjaktarev@gmail.com>
2023-01-16 00:09:07 +0100

    basically redesign the site, need to add media queries for pseudoelements

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ AOS.init({
     offset: 120, // offset (in px) from the original trigger point
     delay: 200, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: true, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 })
@@ -10,50 +10,51 @@ AOS.init({
 AOS.init();
 
 const swiper = new Swiper('.mySwiper', {
-    
+
     loop: true,
 
     pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
+        el: ".swiper-pagination",
+        clickable: true,
+    },
     // Navigation arrows
     navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-}})
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+})
 
 const tabs = document.querySelectorAll('[data-tab-target]')
 const sections = document.querySelectorAll('.section')
 
 tabs.forEach(tab =>
-    tab.addEventListener('click', function() {
+    tab.addEventListener('click', function () {
         const target = document.querySelector(tab.dataset.tabTarget)
         sections.forEach(function (section) {
             section.classList.add('inactive')
             section.classList.remove('active')
-        })    
+        })
         tabs.forEach(function (tab) {
             tab.classList.remove('BG')
         })
         tab.classList.add('BG')
         target.classList.remove('inactive')
         target.classList.add('active')
-        
+
     })
 )
 
-const api_url ="https://randomuser.me/api/?results=5";
+const api_url = "https://randomuser.me/api/?results=5";
 
-async function getapi(url)  {
+async function getapi(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
         updateTestmonials(data)
-    } catch(e) {
+    } catch (e) {
         console.log(e)
     }
-    }
+}
 
 getapi(api_url)
 const quotesArray = ['Brzina kojom ovaj čovjek uči...', 'Nema predanijeg radnika od njega.', 'Ovo nije samo working with APIs demonstracija, ja sam stvarna osoba!', 'Nećete požaliti ako zaposlite ovog čovjeka.', 'Možda tek počinje, ali ima puno potencijala.', 'Timski igrač.', 'Dobro poznaje strane jezike što je uvijek korisno.', 'Pomogao mi je jednom prilikom s kosačicom.', 'A solidan je, šta ja znam...', 'Radi na sebi svaki dan.', 'Čovjek se ne da vidjeti mjesecima, samo uči kodirati po cijele dane.', 'Oprostite, ali ja ne znam ovu osobu.', 'Kao nastavnik bio je jedan od najboljih i najpouzdanijih.', 'Ima divnu ženu.', 'Ovaj čovjek ne zna što je gubljenje vremena.', 'Viđam ga posvuda u web dev zajednici.', 'Kažu da je jednom potrošio cijelu plaću na 50 udemy tečaja.', 'Da citiram Einsteina: "Zakon je."', 'Ima samo jednu manu: ne zna kad treba odustati', 'Pričaju u narodu da mu je toliko dojadio rad u školi da je spreman na sve.', 'Nema baš neki ukus za glazbu, ali što je, tu je.', 'Ne pije, ne puši, nema poroka, osim što kodira po cijele dane.', 'Živi na 4. katu bez lifta pa ima i dobru kondiciju.', 'Njegov je moto: Nemam moto, što će mi moto?', 'Zaljubljenik u povijest.', 'Jedan od najsigurnijih vozača na cesti.', 'E ček ček, to je onaj lik što vodi podcast "Engleski kroz govorne vježbe"?', "Evo, iskreno, ne znam lika, ali čini se okej.", "Čekajte, zašto sam ja ovdje? Što trebam reći?", "Ma stalno ga viđam na WebDevSimplified, ZTM, Kevin Powell i sličnim kanalima na YouTubeu...", "Kažu da se namjerio biti najbolji up-and-coming web developer 2023. godine.", "Toliko puno kodira da je počeo pisati JSON umjesto Jason na nastavi Engleskog.", "Kad čuje Java, prva asocijacija mu nije otok u Indoneziji.", 'E iskače iz paštete više sa svojim GitHub commits i push requests.', 'Kad misli da je sam, nekad samo uzvikne: "GIIIT".', 'Jedan od rijetkih ljudi koji zna ECMAscript 6 napamet...', 'Napravio mi je par polica za kuću, ali nisam ga vidio kako kodira.', 'Taj će vam i gledanje boje kako se suši pretvoriti u natjecanje...', 'Ha čujte... Bože moj, ima nas svakakvih ljudi.', 'Da citiram onu narodnu: "Dokon pop i jariće krsti."', 'Ima jedno 15 godina kad smo zadnji put surađivali, ali nije bilo problema.', 'Rijetko se čujemo ovih dana, a da vam budem iskren, drago mi je da je tako.', 'Veoma emotivno nezahtjevna osoba. Evo, pametnom dosta.', 'Šuć-muć pa prolij...', "Priča se da od ljudi traži verification token kad ih upozna.", "Da ga se vidjeti na indijskim YouTubeu kanalima."]
@@ -82,21 +83,21 @@ function updateTestmonials(data) {
         div.appendChild(name)
         div.appendChild(quote)
         testimonialsSection.appendChild(div)
-      }    
- }
+    }
+}
 
- let images = [...document.querySelectorAll('.project-img')]
+let images = [...document.querySelectorAll('.project-img')]
 
- let modal = [...document.querySelectorAll('[data-modal]')]
- let closeButton = [...document.querySelectorAll('[data-modal-close]')]
+let modal = [...document.querySelectorAll('[data-modal]')]
+let closeButton = [...document.querySelectorAll('[data-modal-close]')]
 
- function selectImagesById(number) {
+function selectImagesById(number) {
     for (let i = 0; i <= number; i++) {
         let imageElement = document.querySelector(`#project${i}`)
         projectImages.push(imageElement)
     }
- }
- 
+}
+
 let projectImages = []
 selectImagesById(11)
 
@@ -106,7 +107,12 @@ for (let i = 0; i < projectImages.length; i++) {
     })
     closeButton[i].addEventListener('click', () => {
         modal[i].close()
-     })
+    })
 }
 
- 
+const randomMessages = ['Why?', 'Stop clicking me!', 'Nothing is going to happen if you keep clicking me...', 'Bro... enough is enough', 'One more click and the call stack will overflow.', "Can't believe you fell for it - LMAO!", "You're one of the only two people who clicked on that.", "I bet you won't click again.", "WHAT DID YOU SAY ABOUT ME YOU LITTLE BI***. I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare..."]
+
+const subtitle = document.querySelector('[data-subtitle]')
+subtitle.addEventListener('click', () => {
+    alert(randomMessages[Math.floor(Math.random() * randomMessages.length)])
+})
